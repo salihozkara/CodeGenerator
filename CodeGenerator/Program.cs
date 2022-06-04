1c9ac886f5798f92edb51df5b354b51e41c50a87
Salih <salihozkara00@gmail.com>
2022-06-05 02:03:29 +0300

    update

--- CodeGenerator/Program.cs
+++ CodeGenerator/Program.cs
@@ -4,8 +4,7 @@
 
 if (args.Length != 3)
 {
-    Console.WriteLine("Usage: {0} <input_file> <input_srcStr> <output_destStr>",
-        Environment.GetCommandLineArgs()[0]);
+    Console.WriteLine($"Usage: { (object)Environment.GetCommandLineArgs()[0]} <input_file> <input_srcStr> <output_destStr>");
     return;
 }
 
@@ -18,11 +17,15 @@ if (sourceString == destString)
     Console.WriteLine("Source and destination strings are the same");
     return;
 }
-
+var fileCount = 0;
+var completeCount = 0;
+var errorCount = 0;
 if (Directory.Exists(sourcePath))
 {
-    var sourceFiles = Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories);
-    var sourceDir = Directory.GetDirectories(sourcePath, "*.*", SearchOption.AllDirectories);
+    var sourceFiles = Directory.GetFiles(sourcePath, "", SearchOption.AllDirectories);
+    fileCount = sourceFiles.Length;
+
+    var sourceDir = Directory.GetDirectories(sourcePath, "", SearchOption.AllDirectories);
     foreach (var dir in sourceDir)
     {
         DirCreator(dir, sourceString, destString);
@@ -40,42 +43,81 @@ else if (File.Exists(sourcePath))
 else
 {
     Console.WriteLine("File or Directory not found");
+    return;
 }
-
+Console.WriteLine("{0} files processed, {1} complete, {2} errors,{3} total", fileCount, completeCount, errorCount, completeCount + errorCount);
 void FileContentSwitcher(string file, string srcStr, string destStr)
 {
-    var fileContent = File.ReadAllText(file);
-    fileContent = fileContent.Replace(srcStr, destStr);
-    fileContent = fileContent.Replace(srcStr.ToTitleCase(), destStr.ToTitleCase());
-    fileContent = fileContent.Replace(srcStr.ToUpper(), destStr.ToUpper());
-    fileContent = fileContent.Replace(srcStr.ToLower(), destStr.ToLower());
-    File.WriteAllText(file, fileContent);
+    try
+    {
+        var fileContent = File.ReadAllText(file);
+        fileContent = fileContent.ReplaceByCase(srcStr, destStr);
+        File.WriteAllText(file, fileContent);
+    }
+    catch (Exception e)
+    {
+        if (e is AccessViolationException)
+        {
+            Console.WriteLine("Access violation exception");
+        }
+        else
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+    }
 }
 
 void FileCreator(string file, string srcStr, string destSrc)
 {
-    var newFile = file.Replace(srcStr, destSrc);
-    newFile = newFile.Replace(srcStr.ToTitleCase(), destSrc.ToTitleCase());
-    newFile = newFile.Replace(srcStr.ToUpper(), destSrc.ToUpper());
-    newFile = newFile.Replace(srcStr.ToLower(), destSrc.ToLower());
-    File.Copy(file, newFile);
-    FileContentSwitcher(newFile, srcStr, destSrc);
+    var newFile = file.ReplaceByCase(srcStr, destSrc);
+    var fileInfo = new FileInfo(newFile);
+
+    try
+    {
+        if (fileInfo.Directory is { Exists: false })
+        {
+            fileInfo.Directory.Create();
+        }
+        File.Copy(file, newFile, true);
+        FileContentSwitcher(newFile, srcStr, destSrc);
+        completeCount++;
+    }
+    catch (Exception e)
+    {
+        errorCount++;
+        Console.WriteLine("File Exception:" + e.Message);
+    }
+
 }
 
 void DirCreator(string dir, string srcStr, string destStr)
 {
-    var newDir = dir.Replace(srcStr, destStr);
-    newDir = newDir.Replace(srcStr.ToTitleCase(), destStr.ToTitleCase());
-    newDir = newDir.Replace(srcStr.ToUpper(), destStr.ToUpper());
-    newDir = newDir.Replace(srcStr.ToLower(), destStr.ToLower());
-    Directory.CreateDirectory(newDir);
+    var newDir = dir.ReplaceByCase(srcStr, destStr);
+    try
+    {
+        Directory.CreateDirectory(newDir);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Directory Exception:"+e.Message);
+    }
 }
 
-
-// string to title extends string
-internal static class StringExtensions
+static class StringExtensions
 {
-    public static string ToTitleCase(this string str)
+    public static string ReplaceByCase(this string str, string oldValue, string newValue)
+    {
+        str = str.Replace(oldValue, newValue);
+        // upper replace
+        str = str.Replace(oldValue.ToUpper(), newValue.ToUpper());
+        // lower replace
+        str = str.Replace(oldValue.ToLower(), newValue.ToLower());
+        // title replace
+        str = str.Replace(oldValue.ToTitle(), newValue.ToTitle());
+        return str;
+    }
+    // ToTitle
+    public static string ToTitle(this string str)
     {
         return new CultureInfo("en-US", false).TextInfo.ToTitleCase(str.ToLower());
     }
